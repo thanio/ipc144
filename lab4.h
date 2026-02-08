2235a5e1b24a24380793ac7ca46d24777c2a3e56
thanio <54902847+thanio@users.noreply.github.com>
2026-02-07 21:53:37 -0500

    Add files via upload

---
+++ lab4.h
@@ -0,0 +1,15 @@
+
+// <TODO: Provide function documentation here>
+int readIntInRange(int min, int max);
+
+// <TODO: Provide function documentation here>
+int getMenuChoice(void);
+
+// <TODO: Provide function documentation here>
+int twoToPowerOfN(int n);
+
+// <TODO: Provide function documentation here>
+int factorial(int n);
+
+// <TODO: Provide function documentation here>
+int fibonacci(int n);
